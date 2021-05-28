b859183b17ee36912ebe476bb0702967ebcc01be
Montlein <montvlein@gmail.com>
2021-05-28 19:12:49 -0300

    ordenada la funcion de movimiento y los fps

--- juego.py
+++ juego.py
@@ -20,6 +20,22 @@ class NaveEspacial(pygame.sprite.Sprite):
 		self.Vida = True
 		self.velocidad = 20
 
+	def mov_left(self):
+		self.rect.left -= self.velocidad
+		self.movimiento()
+
+	def mov_top(self):
+		self.rect.top -= self.velocidad
+		self.movimiento()
+	
+	def mov_right(self):
+		self.rect.right += self.velocidad
+		self.movimiento()
+	
+	def mov_bottom(self):
+		self.rect.bottom += self.velocidad
+		self.movimiento()
+	
 	def movimiento(self):
 		if self.Vida == True:
 			if self.rect.left <=0:
@@ -31,23 +47,24 @@ class NaveEspacial(pygame.sprite.Sprite):
 			elif self.rect.bottom >= alto+20:
 				self.rect.bottom = alto+20
 	
-	def disparar(self):
-		print("pium pium pium!")
+	def disparar(self, x, y):
+		disparo = Proyectil(x,y)
+		self.listaDisparo.append(disparo)
 
 	def dibujar(self, superficie):
 		superficie.blit(self.ImagenNave, self.rect)
 
 class Proyectil(pygame.sprite.Sprite):
 	
-	def __init__(self, posx, posy):
+	def __init__(self, x, y):
 		pygame.sprite.Sprite.__init__(self)
 		self.ImagenProyectil = pygame.image.load("D:\Imagenes/Diseño y creación/Pixel Art/gato.jpg").convert()
 		
 		self.rect = self.ImagenProyectil.get_rect()
 		self.velovidadDisparo = 1
 		
-		self.rect.top = posy
-		self.rect.left = posx
+		self.rect.top = y
+		self.rect.left = x
 
 	def trayectoria(self):
 		self.rect.top -= self.velovidadDisparo
@@ -55,19 +72,23 @@ class Proyectil(pygame.sprite.Sprite):
 	def dibujar(self, superficie):
 		superficie.blit(self.ImagenProyectil, self.rect)
 
+class Enemigo(pygame.sprite.Sprite):
+	pass
+
 def SpaceInvader():
 	pygame.init()
 	ventana = pygame.display.set_mode((ancho,alto))
 	pygame.display.set_caption("Chicken Invader")
 
 	jugador = NaveEspacial()
-	proyectil = Proyectil(ancho/2, alto)
 
 	enJuego = True
+	reloj = pygame.time.Clock()
+
 	while True:
 		ventana.fill((255,255,255))
+		reloj.tick(60)
 		jugador.movimiento()
-		proyectil.trayectoria()
 
 		for evento in pygame.event.get():
 			if evento.type == QUIT:
@@ -77,18 +98,25 @@ def SpaceInvader():
 			if enJuego == True:
 				if evento.type == pygame.KEYDOWN:
 					if evento.key == K_LEFT or evento.key == ord('a'):
-						jugador.rect.left -= jugador.velocidad
+						jugador.mov_left()
 					elif evento.key == K_RIGHT or evento.key == ord('d'):
-						jugador.rect.right += jugador.velocidad
+						jugador.mov_right()
 					elif evento.key == K_UP or evento.key == ord('w'):
-						jugador.rect.top -= jugador.velocidad
+						jugador.mov_top()
 					elif evento.key == K_DOWN or evento.key == ord('s'):
-						jugador.rect.bottom += jugador.velocidad
+						jugador.mov_bottom()
 					elif evento.key == ord(' ') or evento.type == MOUSEBUTTONDOWN:
-						jugador.disparar()
+						x,y = jugador.rect.center
+						jugador.disparar(x,y)
 		
-		proyectil.dibujar(ventana)
 		jugador.dibujar(ventana)
+		if len(jugador.listaDisparo)>0:
+			for x in jugador.listaDisparo:
+				x.dibujar(ventana)
+				x.trayectoria()
+				
+				if x.rect.top < 0:
+					jugador.listaDisparo.remove(x)
 		pygame.display.update()
 
 SpaceInvader()
